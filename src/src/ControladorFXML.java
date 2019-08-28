2c2ad0143280a090cd9f3bfa7c8860fbf349937c
Brayan <alfabrayan12@gmail.com>
2019-08-28 02:38:11 -0600

    Imagenes agregadas al panel y se empezo la prpgramacion del controlador fxml

---
+++ src/src/ControladorFXML.java
@@ -0,0 +1,34 @@
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+
+import java.net.URL;
+
+public class ControladorFXML {
+
+    @FXML
+    private ImageView and;
+    @FXML
+    private ImageView nand;
+    @FXML
+    private ImageView or;
+    @FXML
+    private ImageView nor;
+    @FXML
+    private ImageView not;
+    @FXML
+    private ImageView xor;
+    @FXML
+    private ImageView xnor;
+    @FXML
+    private VBox componentesVbox;
+    @FXML
+    private URL location;
+
+    public ControladorFXML(){
+
+    }
+
+    public void initialize(){
+    }
+}
--- src/src/Interfaz.java
+++ src/src/Interfaz.java
@@ -2,9 +2,12 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URL;
 
 public class Interfaz extends Application {
 
