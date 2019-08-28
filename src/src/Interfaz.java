fcd7d9f174ce117cb8e4e34cc54a56a6fb88af47
Brayan <alfabrayan12@gmail.com>
2019-08-27 22:23:19 -0600

    GUI Primera Iteracion Completa

---
+++ src/src/Interfaz.java
@@ -0,0 +1,26 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Interfaz extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("interfaz.fxml"));
+        Scene scene = new Scene(root, 711, 458);
+        primaryStage.setScene(scene);
+        primaryStage.sizeToScene();
+        primaryStage.show();
+        primaryStage.setMinWidth(primaryStage.getWidth());
+        primaryStage.setMinHeight(primaryStage.getHeight());
+
+    }
+}
