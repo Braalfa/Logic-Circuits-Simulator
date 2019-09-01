0206961235cdd7c48f36238653dafa66e3442436
Braalfa <ahbn5757>
2019-08-31 19:49:01 -0800

    Algoritmo para crear lineas

--- src/src/Tag.java
+++ src/src/Tag.java
@@ -1,5 +1,8 @@
+import javafx.beans.Observable;
+import javafx.collections.ObservableList;
 import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.input.*;
 import javafx.scene.layout.AnchorPane;
@@ -105,7 +108,91 @@ public abstract class Tag extends Label {
 
     }
 
-    public Component getComponent(){
-        return component;
+    private void selfConnect(Point endPoint, Point startPoint){
+        Line[] lines=null;
+        int linesIndex=1;
+        while(lines==null){
+            lines=this.createPath(linesIndex, linesIndex, startPoint.getX(),startPoint.getY(),endPoint.getX(),endPoint.getY(), 1);
+            if (lines==null) {
+                lines = this.createPath(linesIndex, linesIndex, startPoint.getX(), startPoint.getY(), endPoint.getX(), endPoint.getY(), 1);
+            }
+        }
     }
+
+    public Line[] createPath(int numberlines,int orgLines, double ax, double ay, double bx, double by, int direction) {
+        if (numberlines == 1) {
+            if (ax == bx || by == ay) {
+                Line[] result = new Line[orgLines];
+                result[orgLines - 1] = this.createLine(ax,ay,bx,by);
+                return result;
+            }else{
+                return null;
+            }
+        } else {
+            Line[] lines = null;
+            double middle;
+            boolean done = false;
+            double limX= parent.getLayoutX();
+            double limY= parent.getLayoutY();
+            int iterator = 0;
+            Line line =this.createLine(ax,ay,bx,by);
+            if (this.overlaps(line)) {
+                if (direction == 1) {
+                    middle = (by - ay) / 2;
+                    while (lines == null && !done) {
+                        if (ay + middle + iterator < limY) {
+                            lines = createPath(numberlines - 1, orgLines, ax, ay + middle + iterator, bx, by, -1);
+                        }else if (lines == null && (ay + middle - iterator>0)) {
+                            lines = createPath(numberlines - 1, orgLines, ax, ay + middle - iterator, bx, by, -1);
+                        }else{
+                            done=true;
+                        }
+                        iterator++;
+                    }
+                } else {
+                    middle = (bx - ax) / 2;
+                    while (lines == null && !done) {
+                        if (ax + middle + iterator < limX) {
+                            lines = createPath(numberlines - 1, orgLines, ax + middle + iterator, ay, bx, by, -1);
+                        }else if (lines == null && (ax + middle - iterator>0) ) {
+                            lines = createPath(numberlines - 1, orgLines, ax + middle - iterator, ay, bx, by, -1);
+                        }
+                        iterator++;
+                    }
+                }
+                if(lines!=null){
+                    lines[orgLines-numberlines]=line;
+                }
+                return lines;
+            }else{
+                return null;
+            }
+
+        }
+    }
+    private Line createLine(double ax, double ay, double bx, double by){
+        Line line = new Line();
+        line.setStartX(ax);
+        line.setEndX(bx);
+        line.setStartY(ay);
+        line.setEndY(by);
+        line.setVisible(false);
+        parent.getChildren().add(line);
+        return line;
+    }
+    private boolean overlaps(Line line){
+        ObservableList<javafx.scene.Node> nodes=parent.getChildren();
+        boolean result=false;
+        for(Node node: nodes){
+            if (node instanceof Component){
+                if(component.getLayoutBounds().intersects(line.getLayoutBounds())){
+                    result= true;
+                    break;
+                }
+            }
+        }
+        return result;
+
+    }
+
 }
