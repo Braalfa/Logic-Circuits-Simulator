e143da44e85c04033ca808381521f2c8608ad26e
Brayan <alfabrayan12@gmail.com>
2019-09-05 09:46:57 -0600

    Bugs arreglados

--- src/src/Tag.java
+++ src/src/Tag.java
@@ -212,7 +212,7 @@ public abstract class Tag extends Label {
         ObservableList<Node> nodos=parent.getChildren();
         Tag overlap=null;
         for(Node nodo:nodos){
-            if(nodo instanceof Tag && !nodo.getClass().equals(this.getClass())){
+            if(nodo instanceof Tag && !nodo.getClass().equals(this.getClass()) && nodo.isVisible()){
                 if(nodo.getBoundsInParent().intersects(this.getBoundsInParent())){
                     overlap=(Tag)nodo;
                     break;
